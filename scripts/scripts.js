4e96334ae4da8f110656cb8b1deace8f6bf884b9
Olga Krainova <30428922+OKrainova@users.noreply.github.com>
2017-09-07 21:09:42 +0300

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,6 +6,8 @@ $(function () {
         email: /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i,
         tags: /<[^>]+>/gi
     };
+    
+    var menu = $('.header-menu');
 
     $(window).on('scroll', function () {
         $(this).scrollTop() > 100 ? $(".btn-top").fadeIn() : $(".btn-top").fadeOut();
@@ -15,27 +17,47 @@ $(function () {
     $('.btn-top').on('click', function () {
         $('html:not(:animated), body:not(:animated)').animate({scrollTop: 0}, 700);
     });
-
-    $('.header-menu__toggle').on('click', function () {
-
-        var menu = $('.header-menu__list');
-        var self = $(this);
-       
-        self.stop(true).toggleClass('close');
-
-        if (!menu.hasClass('open')) {
-            menu.stop(true).slideDown(500, function(){
-                $(this).addClass('open').removeAttr("style");
-            });
-        } 
-        else
-        {
-           menu.stop(true).slideUp(500, function(){
-               $(this).removeClass('open').removeAttr("style");
-           });
+    
+    $('.nav__toggle').on('click', function(){
+       if(!menu.hasClass('open')){ 
+            menu.addClass('open');
         }
-
+        else {
+            menu.removeClass('open');
+            console.log($(menu).find('.open'));
+           $(menu).find('.open').removeClass('open');
+        }
     });
+        
+    $('.dm-toggle').on('click', function(){
+        $(this).parent('.dropdown').children('.dropdown-menu').addClass('open');
+    });
+    
+    $('.dropdown-menu__item.back').on('click', function(){
+       $(this).parent('.dropdown-menu').removeClass('open'); 
+    });
+    
+
+//    $('.nav__toggle').on('click', function () {
+//
+//        var menu = $('.menu-collapse');
+//        var self = $(this);
+//       
+//        self.stop(true).toggleClass('close');
+//
+//        if (!menu.hasClass('open')) {
+//            menu.stop(true).slideDown(500, function(){
+//                $(this).addClass('open').removeAttr("style");
+//            });
+//        } 
+//        else
+//        {
+//           menu.stop(true).slideUp(500, function(){
+//               $(this).removeClass('open').removeAttr("style");
+//           });
+//        }
+//
+//    });
     
     //проверка формы
     
@@ -52,7 +74,6 @@ $(function () {
                 inputs[i].classList.remove('error');
             }
         }
-
         if (err > 0) {
             e.preventDefault();
         }
